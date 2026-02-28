add54847288cd1e4ed24549b078b99b256acddc8
Aaronkwezi <162333402+Aaronkwezi@users.noreply.github.com>
2026-02-27 22:24:45 -0500

    Add files via upload

---
+++ FMfunc.py
@@ -0,0 +1,8 @@
+def MtoF():
+    meters = float(input("Enter meters:"))
+    feet = meters/0.3048
+    print(round(feet,2),"feet")
+def FtoM():
+    feet = float(input("Enter feet:"))
+    meters=feet*0.3048
+    print(round(meters,2),"meters")
---
+++ STC.py
@@ -0,0 +1,6 @@
+def Stax(tcost):
+    stax=tcost*0.06
+    print("Sales tax:",round(stax,2))
+def TAT(tcost):
+    tat=tcost+(tcost*0.06)
+    print("Sales tax:",round(tat,2))
